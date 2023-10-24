2923f35259a7c53c21381054f03f2fb55f256970
sarah_7qva6ky <husarah2023@gmail.com>
2023-10-24 12:45:30 +0300

    update

--- src/swaglab/myClass.java
+++ src/swaglab/myClass.java
@@ -3,77 +3,41 @@ package swaglab;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class myClass extends parameters {
-	
-	
-	
-	@BeforeTest
+
+	@BeforeMethod
 	public void mySetup() {
 		driver.get(Url);
-		WebElement userNameInputField = driver.findElement(By.id("user-name"));
-		WebElement passNameInputField = driver.findElement(By.id("password"));
-	}
-
-	
-	@Test()
-	public void ALoginWithStandardUSer() throws InterruptedException {
-		WebElement userNameInputField = driver.findElement(By.id("user-name"));
-		userNameInputField.sendKeys(StandardUserName);
-		WebElement passNameInputField = driver.findElement(By.id("password"));
-		passNameInputField.sendKeys(passWord);
-		
-		
-		 
-		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-		driver.findElement(By.id("react-burger-menu-btn")).click();
-		Thread.sleep(3000); 
-
-		driver.findElement(By.id("logout_sidebar_link")).click();
-		
-		
-		
 		
 	}
-		////*[@id="login-button"]
-
 
 	@Test()
-	public void BLoginWithProblemUser() throws InterruptedException {
-		WebElement userNameInputField = driver.findElement(By.id("user-name"));
-		userNameInputField.sendKeys(ProblemUserName);
-		WebElement passNameInputField = driver.findElement(By.id("password"));
-		passNameInputField.sendKeys(passWord);
-		 
-		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-		driver.findElement(By.id("react-burger-menu-btn")).click();
-		Thread.sleep(3000); 
-		driver.findElement(By.id("logout_sidebar_link")).click();
-		
-	
+	public void LoginWithStandardUSer() throws InterruptedException {
+		login(StandardUserName, passWord);
+		// logout();
 
 	}
-	@Test()
-	public void CLoginWithPerformanceUser() throws InterruptedException {
-		WebElement userNameInputField = driver.findElement(By.id("user-name"));
-		userNameInputField.sendKeys(PerformanceUserName);
-		WebElement passNameInputField = driver.findElement(By.id("password"));
-		passNameInputField.sendKeys(passWord);
-		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-		driver.findElement(By.id("react-burger-menu-btn")).click();
-		Thread.sleep(3000); 
 
-		driver.findElement(By.id("logout_sidebar_link")).click();
-		
-		
+	@Test()
+	public void LoginWithProblemUser() throws InterruptedException {
+		login(ProblemUserName, passWord);
+		// logout();
+
+	}
+
+	@Test()
+	public void LoginWithPerformanceUser() throws InterruptedException {
+		login(PerformanceUserName, passWord);
+		// logout();
 
 	}
 
 	@AfterTest
 	public void myPostTesting() {
-		
-		
+
 	}
 }
--- src/swaglab/parameters.java
+++ src/swaglab/parameters.java
@@ -1,10 +1,12 @@
 package swaglab;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class parameters {
-	
+
 	static String Url = "https://www.saucedemo.com/";
 	static String StandardUserName = "standard_user";
 	static String ProblemUserName = "problem_user";
@@ -14,6 +16,25 @@ public class parameters {
 	static String passWord = "secret_sauce";
 
 	static WebDriver driver = new ChromeDriver();
-	 
+
+	static public void login(String username, String password) throws InterruptedException {
+
+		WebElement userNameInputField = driver.findElement(By.id("user-name"));
+		userNameInputField.sendKeys(username);
+		WebElement passNameInputField = driver.findElement(By.id("password"));
+		passNameInputField.sendKeys(passWord);
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		Thread.sleep(3000);
+		driver.get(Url);
+
+	}
+
+//	static public void logout() throws InterruptedException {
+//
+//		driver.findElement(By.id("react-burger-menu-btn")).click();
+//		Thread.sleep(3000);
+//		driver.findElement(By.id("logout_sidebar_link")).click();
+//
+//	}
 
 }
